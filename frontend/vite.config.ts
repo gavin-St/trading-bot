bcc62ef781df1b14f08d31acee0a6ee6ebacf244
Gavin Song <gavins3math@gmail.com>
2025-01-10 03:04:13 -0500

    deploy bugfix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
 export default defineConfig({
-  base: '/trading-bot/frontend/',
+  base: '/trading-bot',
   build: {
     outDir: 'dist', 
   },
