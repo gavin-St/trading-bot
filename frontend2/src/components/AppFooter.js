924f80a9c58543f55a66870c29c82d59751131ec
Gavin Song <gavins3math@gmail.com>
2025-02-18 02:28:29 -0500

    new frontend commit

---
+++ frontend2/src/components/AppFooter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { CFooter } from '@coreui/react'
+
+const AppFooter = () => {
+  return (
+    <CFooter className="px-4">
+      <div>
+        <span className="ms-1">Gavin &copy; 2025</span>
+      </div>
+      <div className="ms-auto">
+        <span className="me-1">Powered by</span>
+        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
+          CoreUI
+        </a>
+      </div>
+    </CFooter>
+  )
+}
+
+export default React.memo(AppFooter)
